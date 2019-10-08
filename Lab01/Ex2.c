f5d5fd1850c2d9ba3de758f8359526c6dfdaaba6
259105 <50930924+259105@users.noreply.github.com>
2019-10-08 23:33:54 +0200

    Add files via upload

---
+++ Lab01/Ex2.c
@@ -0,0 +1,101 @@
+//
+// Created by s259105 on 06/10/2019.
+// Finished on 08/10/2019
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define Nf 200
+#define Np 25
+#define maxdiz 30
+
+void ricodifica(char frase[],int codice[],char parole[][Np+1],int n);
+int cerca(char parole[][Np+1],char frase[],int n,int pos);
+void sostituisci(int pos, char frase[],char parola[][Np+1],int ricod[],int n);
+
+int main(){
+    char frase[Nf+1],parole[maxdiz][Np+1];
+    int i,n,codice[maxdiz];
+    FILE *srg, *ric, *diz;
+
+    if((srg=fopen("sorgente.txt","r"))==NULL){
+        fprintf(stderr,"Errore apertura file SORGENTE");
+        return 0;
+    }
+    if((ric=fopen("ricodificato.txt","w"))==NULL){
+        fprintf(stderr,"Errore apertura file di scrittura.");
+        return 0;
+    }
+    if((diz=fopen("dizionario.txt","r"))==NULL){
+        fprintf(stderr,"Errore apertura file DIZIONARIO");
+        return 0;
+    }
+
+    fscanf(diz,"%d",&n);
+    for(i=0;i<n;i++){
+        fscanf(diz,"\n$%d$ %s",&codice[i],parole[i]);
+    }
+
+    while(fgets(frase,200,srg)!=NULL){
+        ricodifica(frase,codice,parole,n);
+        fprintf(ric,"%s",frase);
+    }
+    fclose(srg);
+}
+
+void ricodifica(char frase[],int codice[],char parole[][Np+1],int n){
+    int i,posizione=0;
+    for(i=0;i<n;i++){//Questo contatore lo uso per cambiare ogni volta parola da controllare nella frase
+                    //Faccio un ciclo while cosi da poter controllare più volte la stessa parola nella stessa frase
+                    //il while finisce quando non si trovano sostituzioni.
+        while ((posizione = cerca(parole, frase,i,posizione))) {//Passo anche la posizone cosi quando va a continuare
+                                                                //il nuovo ciclo non riparte da zero ma continua con la
+                                                                //scansione iniale, facendo cosi si riduce di molto il
+                                                                //numero di iterazioni
+            sostituisci(posizione, frase, parole, codice,i);
+        }
+    }
+}
+
+int cerca(char parole[][Np+1],char frase[],int n,int pos){
+    int i,j,k,l_frase,l_parola;
+    //i=cont. che mi indica in che indice mi trovo nella stringa "frase"
+    //j parte da i e itera per tante volte quanto è lunga la "parola" da trovare, questo mi serve per confrontare
+    //la sottostringa con la parola del dizionario, che viene invece contata lettera per lettera da k.
+    l_frase=strlen(frase);
+    l_parola=strlen(parole[n]);
+    for(i=pos;i<=(l_frase-l_parola);i++){ //la condizione l'ho scelta cosi nonostante esce fuori dall sotto-vettore di
+                                        //interesse per evitare eventuale errori di tipo \n o \0
+        for(j=i,k=0;j<=i+l_parola-1;j++) { //-1 perche il contatore parte da 0, mentre la lunghezza della parola è
+                                            //contata partendo da 1
+            if(parole[n][k]==frase[j]){
+                k++;
+                if(k==l_parola){
+                    return i;  //restituisco il contatore i che mi indica quando inzia la parte di parola da sostituire
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+void sostituisci(int pos, char frase[],char parola[][Np+1],int ricod[],int n){
+    int i,j,l_sostituzione,l_parola;
+    char sostituzione[10],frase_supporto[Nf];
+    //i= usato come cont. nella stringa "frase"
+    //j= usato come cont. nella stringa "sostituzione"
+    sprintf(sostituzione,"$%d$",ricod[n]);
+    strcpy(frase_supporto,frase); //mi salvo la frase per non avere perdita di dati, come ad esempio avere una
+                                //sostituzione troppo lunga può provocare perdita di dati.
+    l_sostituzione=strlen(sostituzione);
+    l_parola=strlen(parola[n]);
+
+    for(i=pos,j=0;j<l_sostituzione;i++,j++){ //sostituisco la parte di parola con il codice
+        frase[i]=sostituzione[j];
+    }
+    while (i<Nf){  //proseguo sempre dallo stesso indice i fino a fine vettore copiando la frase di supporto.
+        frase[i]=frase_supporto[i-l_sostituzione+l_parola];
+        i++;
+    }
+}
---
+++ Lab01/Ex3.c
@@ -0,0 +1,168 @@
+//
+// Created by s259105 on 08/10/2019.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX_MATRICE 300
+#define MAX_FILE 20
+#define MAX_COMADA 100
+
+void inizializza_matrice(int matrice[][MAX_MATRICE],int n);
+void apri_file_salva_matrice(int matrice[][MAX_MATRICE],int *nr,int *nc);
+void stampa_matrice(int matrice[][MAX_MATRICE],int nr,int nc);
+int menu(int *selettore,int *indice,int *direzione,int *posizione);
+void ruota(int M[][MAX_MATRICE],int nr,int nc,int selettore, int direzione,int indice,int posizione);
+
+//selettore 1=riga 2=colonna
+//direzione -1=destra/su 1=sinistra/giu
+int main(void){
+    int M[MAX_MATRICE][MAX_MATRICE],nr,nc,selettore,indice,direzione,posizioni;
+
+    inizializza_matrice(M,0);
+    apri_file_salva_matrice(M,&nr,&nc);
+    stampa_matrice(M,nr,nc);
+    while(menu(&selettore,&indice,&direzione,&posizioni)){
+        ruota(M,nr,nc,selettore,direzione,indice,posizioni);
+        stampa_matrice(M,nr,nc);
+    }
+}
+
+void inizializza_matrice(int matrice[][MAX_MATRICE],int n){
+    int i,j;
+
+    for(i=0;i<MAX_MATRICE;i++){
+        for(j=0;j<MAX_MATRICE;j++){
+            matrice[i][j]=n;
+        }
+    }
+}
+
+void stampa_matrice(int matrice[][MAX_MATRICE],int nr,int nc){
+    int i,j;
+
+    for(i=0;i<nr;i++){
+        for(j=0;j<nc;j++){
+            printf("%d",matrice[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void apri_file_salva_matrice(int matrice[][MAX_MATRICE],int *nr,int *nc){
+    int i,j;
+    char file[MAX_FILE];
+    FILE *fp;
+
+    printf("Inserisci il nome del file da aprire (MAX 20 caratteri):\n");
+    scanf("%s",file);
+    while((fp=fopen(file,"r"))==NULL){
+        fprintf(stderr,"Errore hai inserito il nome del file errato o troppo grande, riprova\n");
+        printf("Inserisci il nome del file da aprire (MAX 20 caratteri):\n");
+        scanf("%s",file);
+    }
+
+    fscanf(fp,"%d %d",nr,nc);
+    for(i=0;i<*nr;i++){
+        for(j=0;j<*nc;j++){
+            fscanf(fp,"%d",&matrice[i][j]);
+        }
+    }
+}
+
+int menu(int *selettore,int *indice,int *direzione,int *posizione){
+    char sel[MAX_FILE],dir[MAX_FILE];
+    int i;
+
+        printf("Inserisci comanda  nella forma  <selettore> <indice> <direzione> <posizioni>:\n"
+               "<selettore>= 'riga', 'colonna' o 'fine';\n"
+               "<indice>= quale indice della riga/colonna vuoi far muovere?\n"
+               "<direzione>= 'destra' o 'sinistra' per le righe, 'su' o 'giu' per le colonne;\n"
+               "<posizioni>= Inidicare di quante posizioni vuoi far compiere la rotazione.\n");
+        scanf("%s", sel);
+        if (strcmp(sel, "fine") == 0) {
+            return 0;
+        }
+        scanf("%d %s %d", indice, dir, posizione);
+        for (i = 0; i < MAX_FILE; i++) {
+            sel[i] = tolower(sel[i]);
+            dir[i] = tolower(dir[i]);
+        }
+        if (strcmp(sel, "riga")==0) {
+            *selettore = 1;
+            if (strcmp(dir, "destra") == 0) {
+                *direzione = -1;
+                return 1;
+            } else if (strcmp(dir, "sinistra")==0) {
+                *direzione = 1;
+                return 1;
+            }
+        } else {
+            *selettore = 2;
+            if(strcmp(dir,"su")==0){
+                *direzione=1;
+                return 1;
+            }else if(strcmp(dir,"giu")==0){
+                *direzione=-1;
+                return 1;
+            }
+        }
+    return 0;
+}
+
+void ruota(int M[][MAX_MATRICE],int nr,int nc,int selettore, int direzione,int indice,int posizione){
+    int i,j,V[MAX_MATRICE];
+    //Vn è il vettore nuovo ovvero il vettore che contiene la "rotazione"
+    //V il vettore iniziale non ruotato
+    nc--;nr--;//prendo in considerzione che inizio a contare da 0;
+    indice--;// tengo in considerazione che devo contare da 0 in poi
+
+
+
+    //selettore 1=riga 2=colonna
+    //direzione -1=destra/giu 1=sinistra/su
+
+    if(selettore==1){
+        //Riga
+        if(posizione>nc){
+            posizione=posizione%(nc+1);//Faccio +1 perche mi servono le righe effettive e non quelle contate a partire
+                                        // da 0.
+        }
+        if(direzione==1){
+            posizione=nc+1-posizione; //Trasformo la direzione da sinistra a destra.
+        }
+        for(i=0,j=0;i<=nc;i++){          //Prendo il vettore nella matrice che mi interessa
+            if(i<=nc-posizione)
+                V[i+posizione]=M[indice][i]; //Sposto dal primo fino al n-spostamento
+            else {
+                V[j] = M[indice][i]; // sposto dal n-spostamento all'ultimo
+                j++;
+            }
+        }
+        for(i=0;i<=nc;i++){ // Ricopio il vettore creato
+            M[indice][i]=V[i];
+        }
+    }else{
+        //Colonna
+        if(posizione>nr){
+            posizione=posizione%(nr+1);//Faccio +1 perche mi servono le righe effettive e non quelle contate a partire
+            // da 0.
+        }
+        if(direzione==1){
+            posizione=nr+1-posizione; //Trasformo la direzione da su a giu.
+        }
+        for(i=0,j=0;i<=nr;i++){          //Prendo il vettore nella matrice che mi interessa
+            if(i<=nr-posizione)
+                V[i+posizione]=M[i][indice]; //Sposto dal primo fino al n-spostamento
+            else {
+                V[j] = M[i][indice]; // sposto dal n-spostamento all'ultimo
+                j++;
+            }
+        }
+        for(i=0;i<=nr;i++){ // Ricopio il vettore creato
+            M[i][indice]=V[i];
+        }
+    }
+}
