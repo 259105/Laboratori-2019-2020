8b1716070a2557de99e28cc9a59593dc8fb8dffc
259105 <50930924+259105@users.noreply.github.com>
2019-10-09 17:05:58 +0200

    Update Ex3.c

--- Lab01/Ex3.c
+++ Lab01/Ex3.c
@@ -8,13 +8,12 @@
 #include <ctype.h>
 #define MAX_MATRICE 300
 #define MAX_FILE 20
-#define MAX_COMADA 100
 
 void inizializza_matrice(int matrice[][MAX_MATRICE],int n);
 void apri_file_salva_matrice(int matrice[][MAX_MATRICE],int *nr,int *nc);
 void stampa_matrice(int matrice[][MAX_MATRICE],int nr,int nc);
 int menu(int *selettore,int *indice,int *direzione,int *posizione);
-void ruota(int M[][MAX_MATRICE],int nr,int nc,int selettore, int direzione,int indice,int posizione);
+void ruota(int M[][MAX_MATRICE],int n,int selettore, int direzione,int indice,int posizione);
 
 //selettore 1=riga 2=colonna
 //direzione -1=destra/su 1=sinistra/giu
@@ -25,7 +24,10 @@ int main(void){
     apri_file_salva_matrice(M,&nr,&nc);
     stampa_matrice(M,nr,nc);
     while(menu(&selettore,&indice,&direzione,&posizioni)){
-        ruota(M,nr,nc,selettore,direzione,indice,posizioni);
+        if(selettore==1)
+            ruota(M,nc,selettore,direzione,indice,posizioni);
+        else
+            ruota(M,nr,selettore,direzione,indice,posizioni);
         stampa_matrice(M,nr,nc);
     }
 }
@@ -86,7 +88,7 @@ int menu(int *selettore,int *indice,int *direzione,int *posizione){
             return 0;
         }
         scanf("%d %s %d", indice, dir, posizione);
-        for (i = 0; i < MAX_FILE; i++) { //Questo serve per non avere problemi di maiuscolo o minuscolo
+        for (i = 0; i < MAX_FILE; i++) {
             sel[i] = tolower(sel[i]);
             dir[i] = tolower(dir[i]);
         }
@@ -112,58 +114,38 @@ int menu(int *selettore,int *indice,int *direzione,int *posizione){
     return 0;
 }
 
-void ruota(int M[][MAX_MATRICE],int nr,int nc,int selettore, int direzione,int indice,int posizione){
+void ruota(int M[][MAX_MATRICE],int n,int selettore, int direzione,int indice,int posizione){
     int i,j,V[MAX_MATRICE];
     //Vn è il vettore nuovo ovvero il vettore che contiene la "rotazione"
-    //V il vettore iniziale non ruotato
-    nc--;nr--;//prendo in considerzione che inizio a contare da 0;
+    if(posizione>n){
+        posizione=posizione%(n);
+    }
+    if(direzione==1){
+        posizione=n-posizione; //Trasformo la direzione da su a giu oppure da sinistra a destra
+    }
+    n--;//cosi è il numero di righe se comincio a contare da 0.
     indice--;// tengo in considerazione che devo contare da 0 in poi
-
-
 
     //selettore 1=riga 2=colonna
     //direzione -1=destra/giu 1=sinistra/su
-
-    if(selettore==1){
-        //Riga
-        if(posizione>nc){ //Questo serve in caso ci sono dei spostamenti maggiori della matrice stessa
-                            //Prendo il resto della divisione rotazione_tot_posizione/lunghezza_riga/colonna
-            posizione=posizione%(nc+1);//Faccio +1 perche mi servono le righe effettive e non quelle contate a partire
-                                        // da 0.
+    for(i=0,j=0;i<=n;i++){          //Prendo il vettore nella matrice che mi interessa
+        if(i<=n-posizione)
+            if(selettore==1)        //Sposto dal primo fino al n-spostamento
+                V[i+posizione]=M[indice][i];
+            else
+                V[i+posizione]=M[i][indice];
+        else {
+            if(selettore==1) // sposto dal n-spostamento all'ultimo
+                V[j] = M[indice][i];
+            else
+                V[j] = M[i][indice];
+            j++;
         }
-        if(direzione==1){
-            posizione=nc+1-posizione; //Trasformo la direzione da sinistra a destra.
-        }
-        for(i=0,j=0;i<=nc;i++){          //Prendo il vettore nella matrice che mi interessa
-            if(i<=nc-posizione)
-                V[i+posizione]=M[indice][i]; //Sposto dal primo fino al n-spostamento
-            else {
-                V[j] = M[indice][i]; // sposto dal n-spostamento all'ultimo
-                j++;
-            }
-        }
-        for(i=0;i<=nc;i++){ // Ricopio il vettore creato
+    }
+    for(i=0;i<=n;i++){// Ricopio il vettore creato
+        if(selettore==1)
             M[indice][i]=V[i];
-        }
-    }else{
-        //Colonna
-        if(posizione>nr){
-            posizione=posizione%(nr+1);//Faccio +1 perche mi servono le righe effettive e non quelle contate a partire
-            // da 0.
-        }
-        if(direzione==1){
-            posizione=nr+1-posizione; //Trasformo la direzione da su a giu.
-        }
-        for(i=0,j=0;i<=nr;i++){          //Prendo il vettore nella matrice che mi interessa
-            if(i<=nr-posizione)
-                V[i+posizione]=M[i][indice]; //Sposto dal primo fino al n-spostamento
-            else {
-                V[j] = M[i][indice]; // sposto dal n-spostamento all'ultimo
-                j++;
-            }
-        }
-        for(i=0;i<=nr;i++){ // Ricopio il vettore creato
+        else
             M[i][indice]=V[i];
-        }
     }
 }
